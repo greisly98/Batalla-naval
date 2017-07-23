98ea9705b9b8927cd5ae13df6a9b68c62f8c42f0
greisly98 <gdal_2@hotmail.com>
2017-07-23 06:39:32 -0430

    en este commit, se puede ver como se le hicieron algunos cambios al campo,y tambien se le valido para que el barco fuese seguido (osea, las coordenadas quedan una detras de la otra).

--- batalla naval.cpp
+++ batalla naval.cpp
@@ -2,136 +2,167 @@
 #include <conio.h>
 #include <stdlib.h>
 #include <time.h>
-int MatrizInicial() // Funcion para los Tableros.
-{   int m=0,n=0,x=0,y=0;
-    int MatrizPc[m][n], MatrizJ[m][n];
 
-    printf(" \n");
-    printf("          \t    Bienvenido a Batalla Naval \n\n");
-    
-    //MatrizPc
-    
-    printf("\t| A || B || C || D || F || G || H || I || J || K | \n");
-    printf("\t=================================================== \n");
-	printf("");
-    
-    for(x=0;x<10;x++){
-    	printf("   %d",x);
-    	printf("\t");
-    	for(y=0;y<10;y++){
-    	printf("|~~~|", MatrizPc[x][y]);
-    	
-		}
-		         	printf("\n");
-	}
-    printf("\t===================================================\n");
-    
-    printf(" \n"); // Separador de Tableros
-        printf("\t*************************************************** \n"); // Separador de Tableros
-	printf(" \n"); // Separador de Tableros
-    
-    //MatrizJugador
-    
-    printf("\t| A || B || C || D || F || G || H || I || J || K | \n");
-    printf("\t=================================================== \n");
-	printf("");
-    
-    for(x=0;x<10;x++){
-    	printf("   %d",x);
-    	printf("\t");
-    	for(y=0;y<10;y++){
-    	printf("|~~~|", MatrizJ[x][y]);
-    	
-		}
-		         	printf("\n");
-	}
-    printf("\t===================================================\n");
-    printf(" \n");
-    
-} 
-
+void MatrizInicial(char[][10],char[][10]);
+void AddBarco (char MatrizPc[][10],char MatrizJ[][10]);
 
 void ListaBarcos () { // Barcos segun reglas Hasbro.
-    int f=0,c=0;
-	int PortaAviones[f][c]; // 5 espacios.
-	int Acorazado1[f][c]; // 4 espacios.
-	int Acorazado2[f][c]; // 4 espacios.
-	int Destructor1[f][c]; // 3 espacios.
-	int Destructor2[f][c]; // 3 espacios.
-	int Destructor3[f][c]; // 3 espacios.
-	int Submarino1[f][c]; // 3 espacios.
-	int Submarino2[f][c]; // 3 espacios.
-	int Submarino3[f][c]; // 3 espacios.
-	int Fragata[f][c]; // 2 espacios.
-    }
+    int f=10,c=10;
+    char PortaAviones2[f][c]; // 5 espacios.
+	char PortaAviones[f][c]; // 5 espacios.
+	char Acorazado1[f][c]; // 4 espacios.
+	char Acorazado2[f][c]; // 4 espacios.
+	char Destructor1[f][c]; // 3 espacios.
+	char Destructor2[f][c]; // 3 espacios.
+	char Submarino1[f][c]; // 3 espacios.
+	char Submarino2[f][c]; // 3 espacios.
+	char Fragata1[f][c]; // 2 espacios.
+	char Fragata2[f][c]; // 2 espacios.
+}
     
     
-void PortaAviones (){ // Funcion para Porta-Aviones.
-    int m=0,n=0,x=0,y=0;
-    int MatrizJ[m][n];
+void AddBarco (char MatrizPc[][10],char MatrizJ[][10]){ // Funcion para Porta-Aviones.
     int s=10,a=10,X=0,Y=0,i=0,j=0,coordenadas=0,coordenadas1[s][a];
 	int t=0,NumNave=0, Pop=0,Bandera=0,MiMatriz[s][a];
+	
 	char LetraNave[2];
+    
      	
 	printf("\nUbica a tu PortaAviones. \n Esta nave tiene 5 espacios.\n");
 	printf("  Ingresa las coordenadas de esta Nave.\n\n");
 	
 	for (t=0;t<5;t++){
-
-	do{
-  	printf("Ingresa un numero para la fila \nen la que estara el Porta-Aviones:");
-  	if (scanf(" %d",&NumNave)==1){
-  		Bandera=Bandera+1;
-	  }
-	  else {
-	  	fflush(stdin);
-	  	printf("Error, solo debes ingresar numeros.");
-	  }
-}while (Bandera==0);
-    printf("\n");     
-	         
-    Bandera=0;
-	do{
-  	printf("Ingresa un dfrgo para la fila \nen la que estara el Porta-Aviones:");
-  	if (scanf(" %d",&Y)==1){
-  		Bandera=Bandera+1;
-	  }
-	  else {
-	  	fflush(stdin);
-	  	printf("Error, solo numero.");
-	  }
-}while (Bandera==0);
+    int repetir=0;
+    	
+    	do{
+            repetir=0;
+            do{
+              	printf("Ingresa un numero para la fila \nen la que estara el Porta-Aviones:");
+          	    if (scanf(" %d",&NumNave)==1){
+          		    Bandera=Bandera+1;
+        	    }
+        	    else {
+        	  	    fflush(stdin);
+        	  	    printf("Error, solo debes ingresar numeros.");
+        	    }
+            }while (Bandera==0);
+            printf("\n");     
+        	         
+            Bandera=0;
+        	do{
+          	     printf("Ingresa un numero para la columna \nen la que estara el Porta-Aviones:");
+          	     if (scanf(" %d",&Y)==1){
+          		    Bandera=Bandera+1;
+        	       }
+        	else {
+        	  	fflush(stdin);
+        	  	printf("Error, solo numeros.");
+        	}
+            }while (Bandera==0);
+            
+            if(t==0){
+                MatrizJ[NumNave-1][Y-1]='X';     
+                coordenadas1[Y][NumNave]='X';
+            }
+            
+            else if(t>0 && (MatrizJ[NumNave-2][Y-1]=='X' || MatrizJ[NumNave][Y-1]=='X' || MatrizJ[NumNave-1][Y-2]=='X' || MatrizJ[NumNave-1][Y]=='X'))
+            {
+                MatrizJ[NumNave-1][Y-1]='X';
+                coordenadas1[Y-1][NumNave-1]='X';
+            }else{
+                printf("las posiciones deben ser seguidas, por favor repita la coordenada");
+                repetir=1;
+            }
+            
+                    
+        }while(repetir==1);
+        printf("\n");       
         
-	MatrizJ[NumNave][Y]=1;
-	t++;
-    printf("\n");  
-	
-	MiMatriz[NumNave-1][Y-1]='X';     
-    coordenadas1[NumNave][Y]='X';
-    coordenadas=coordenadas+1;
-    srand(time(NULL));  
-	system("cls");
-}
+        coordenadas=coordenadas+1;
+        srand(time(NULL));  
+    	system("cls");
+    }
 }	
 	
 
-main(){ // Inicio del Juego.
+int main(int argc, char *argv[]){ // Inicio del Juego.
+    char MatrizPc[10][10],MatrizJ[10][10];
+	int CasillasBarcos[5];
 	
+	CasillasBarcos[0]=2;
+    CasillasBarcos[1]=3;
+    CasillasBarcos[2]=3;
+    CasillasBarcos[3]=4;
+    CasillasBarcos[4]=5;
+	
+	
+    for(int x=0;x<10;x++){
+        for(int y=0;y<10;y++){
+            MatrizPc[x][y]=' ';
+            MatrizJ[x][y]=' ';
+        }
+    }
 	int X=0, Y=0;
 	int s=10, a=10;
-	MatrizInicial();
+	MatrizInicial(MatrizPc,MatrizJ);
 	printf("          \tEs momento de posicionar tus barcos!\n");
 	printf("          \tPiensa sabiamente!");
 	printf("\n");
 	printf("          \t    ");
 	
-	PortaAviones (); // Llamada de la funcion Porta-Aviones
+	AddBarco (MatrizPc,MatrizJ); // Llamada de la funcion Porta-Aviones
 	printf("Porta-Aviones listo!!!");
     system("PAUSE");
 	system("cls")	;
 	
-
+    MatrizInicial(MatrizPc,MatrizJ);
     system("PAUSE");
     return EXIT_SUCCESS;
 }
 
+void MatrizInicial(char MatrizPc[][10],char MatrizJ[][10]){ // Funcion para los Tableros.   
+    
+    
+    printf(" \n");
+    printf("          \t    Bienvenido a Batalla Naval \n\n");
+    
+    //MatrizPc
+    
+    printf("\t|A||B||C||D||F||G||H||I||J||K|\n");
+    printf("\t=============================== \n");
+	printf("");
+    
+    for(int x=0;x<10;x++){
+    	printf("   %d",x+1);
+    	printf("\t");
+    	for(int y=0;y<10;y++){
+    	   printf("|%c|", MatrizPc[x][y]);
+    	
+		}
+		printf("\n");
+	}
+    printf("\t===============================\n");
+    
+    printf(" \n"); // Separador de Tableros
+    printf("\t******************************* \n"); // Separador de Tableros
+	printf(" \n"); // Separador de Tableros
+    
+    //MatrizJugador
+    
+    printf("\t|A||B||C||D||F||G||H||I||J||K|\n");
+    printf("\t=============================== \n");
+	printf("");
+    
+    for(int x=0;x<10;x++){
+    	printf("   %d",x+1);
+    	printf("\t");
+    	for(int y=0;y<10;y++){
+    	   printf("|%c|", MatrizJ[x][y]);
+    	
+		}
+	    printf("\n");
+	}
+    printf("\t===============================\n");
+    printf(" \n");
+    
+}
